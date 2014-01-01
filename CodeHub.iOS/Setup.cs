2f352b961ae11640691a17956d9ec4fb00babfc1
Dillon Buchanan <thedillonb@gmail.com>
2014-01-01 11:22:30 -0500

    A few fixes after the change in serialization library

--- CodeHub.iOS/Setup.cs
+++ CodeHub.iOS/Setup.cs
@@ -14,6 +14,7 @@ using CodeFramework.iOS;
 using MonoTouch.Dialog;
 using CodeFramework.iOS.Views;
 using MonoTouch.UIKit;
+using CodeFramework.iOS.Bindings;
 
 namespace CodeHub.iOS
 {
@@ -99,53 +100,4 @@ namespace CodeHub.iOS
             return new Core.App();
         }
     }
-
-	public class UISegmentControlBinding : Cirrious.MvvmCross.Binding.Bindings.Target.MvxTargetBinding
-	{
-		private readonly UISegmentedControl _ctrl;
-
-		public UISegmentControlBinding(UISegmentedControl ctrl)
-			: base(ctrl)
-		{
-			this._ctrl = ctrl;
-		}
-
-		public override void SetValue(object value)
-		{
-			_ctrl.SelectedSegment = (int)value;
-		}
-		public override void SubscribeToEvents()
-		{
-			_ctrl.ValueChanged += HandleValueChanged;
-		}
-
-		void HandleValueChanged (object sender, EventArgs e)
-		{
-			FireValueChanged(_ctrl.SelectedSegment);
-		}
-
-		public override Type TargetType
-		{
-			get
-			{
-				return typeof(int);
-			}
-		}
-		public override Cirrious.MvvmCross.Binding.MvxBindingMode DefaultMode
-		{
-			get
-			{
-				return Cirrious.MvvmCross.Binding.MvxBindingMode.TwoWay;
-			}
-		}
-
-		protected override void Dispose(bool isDisposing)
-		{
-			if (isDisposing)
-			{
-				_ctrl.ValueChanged -= HandleValueChanged;
-			}
-			base.Dispose(isDisposing);
-		}
-	}
 }
