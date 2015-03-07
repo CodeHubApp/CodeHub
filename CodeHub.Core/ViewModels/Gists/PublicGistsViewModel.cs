47cdb6cbc1ece30067f25d556ef7ff7de2827e01
Dillon Buchanan <thedillonb@gmail.com>
2015-03-07 16:39:09 -0500

    Remove searchbar from PublicGistView

--- CodeHub.Core/ViewModels/Gists/PublicGistsViewModel.cs
+++ CodeHub.Core/ViewModels/Gists/PublicGistsViewModel.cs
@@ -1,20 +1,56 @@
-﻿using CodeHub.Core.Services;
+﻿using System;
+using GitHubSharp.Models;
+using ReactiveUI;
+using System.Linq;
+using GitHubSharp;
+using System.Reactive;
+using CodeHub.Core.Services;
 
 namespace CodeHub.Core.ViewModels.Gists
 {
-    public class PublicGistsViewModel : BaseGistsViewModel
+    public class PublicGistsViewModel : BaseViewModel, ILoadableViewModel, IPaginatableViewModel
     {
-        private readonly ISessionService _applicationService;
+        public IReadOnlyReactiveList<GistItemViewModel> Gists { get; private set; }
 
-        public PublicGistsViewModel(ISessionService applicationService)
+        public IReactiveCommand<Unit> LoadCommand { get; private set; }
+
+        private IReactiveCommand<Unit> _loadMoreCommand;
+        public IReactiveCommand<Unit> LoadMoreCommand
         {
-            _applicationService = applicationService;
-            Title = "Public Gists";
+            get { return _loadMoreCommand; }
+            private set { this.RaiseAndSetIfChanged(ref _loadMoreCommand, value); }
         }
 
-        protected override GitHubSharp.GitHubRequest<System.Collections.Generic.List<GitHubSharp.Models.GistModel>> CreateRequest()
+        public PublicGistsViewModel(ISessionService sessionService)
         {
-            return _applicationService.Client.Gists.GetPublicGists();
+            Title = "Public Gists";
+
+            var gists = new ReactiveList<GistModel>();
+            Gists = gists.CreateDerivedCollection(x => CreateGistItemViewModel(x));
+
+            LoadCommand = ReactiveCommand.CreateAsyncTask(async t =>
+            {
+                var request = sessionService.Client.Gists.GetPublicGists();
+                await gists.SimpleCollectionLoad(request, t as bool?, 
+                    x => LoadMoreCommand = x == null ? null : ReactiveCommand.CreateAsyncTask(_ => x()));
+            });
+        }
+
+        private GistItemViewModel CreateGistItemViewModel(GistModel gist)
+        {
+            var title = (gist.Owner == null) ? "Anonymous" : gist.Owner.Login;
+            var description = string.IsNullOrEmpty(gist.Description) ? "Gist " + gist.Id : gist.Description;
+            var imageUrl = (gist.Owner == null) ? null : gist.Owner.AvatarUrl;
+            if (gist.Files.Count > 0)
+                title = gist.Files.First().Key;
+
+            return new GistItemViewModel(title, imageUrl, description, gist.UpdatedAt, _ =>
+            {
+                var vm = this.CreateViewModel<GistViewModel>();
+                vm.Id = gist.Id;
+                vm.Gist = gist;
+                NavigateTo(vm);
+            });
         }
     }
 }
--- CodeHub.iOS/Views/Gists/PublicGistsView.cs
+++ CodeHub.iOS/Views/Gists/PublicGistsView.cs
@@ -1,8 +1,23 @@
+using System;
 using CodeHub.Core.ViewModels.Gists;
+using CodeHub.iOS.TableViewSources;
+using CodeHub.iOS.ViewComponents;
+using UIKit;
 
 namespace CodeHub.iOS.Views.Gists
 {
-    public class PublicGistsView : BaseGistsView<PublicGistsViewModel>
+    public class PublicGistsView : BaseTableViewController<PublicGistsViewModel>
     {
+        public PublicGistsView()
+        {
+            EmptyView = new Lazy<UIView>(() =>
+                new EmptyListView(Octicon.Gist.ToEmptyListImage(), "There are no gists."));
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            TableView.Source = new GistTableViewSource(TableView, ViewModel.Gists);
+        }
     }
 }
