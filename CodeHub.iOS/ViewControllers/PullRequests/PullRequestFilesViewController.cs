bc5291b56928505ade3961f079671a26d67ee5f3
Dillon Buchanan <thedillonb@gmail.com>
2015-08-30 00:25:23 -0400

    Files will appear instantly (PullRequestFilesViewController)

--- CodeHub.iOS/ViewControllers/PullRequests/PullRequestFilesViewController.cs
+++ CodeHub.iOS/ViewControllers/PullRequests/PullRequestFilesViewController.cs
@@ -10,24 +10,19 @@ namespace CodeHub.iOS.ViewControllers.PullRequests
 {
     public class PullRequestFilesViewController : BaseTableViewController<PullRequestFilesViewModel>
     {
-        public PullRequestFilesViewController()
-        {
-            EmptyView = new Lazy<UIView>(() =>
-                new EmptyListView(Octicon.FileCode.ToEmptyListImage(), "There are no files."));
-        }
-
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
 
+            EmptyView = new Lazy<UIView>(() =>
+                new EmptyListView(Octicon.FileCode.ToEmptyListImage(), "There are no files."));
+
             var notificationSource = new CommitedFilesTableViewSource(TableView);
             TableView.Source = notificationSource;
 
-            this.WhenActivated(d => {
-                d(this.WhenAnyValue(x => x.ViewModel.Files)
-                    .Merge(this.WhenAnyObservable(x => x.ViewModel.Files.Changed).Select(_ => ViewModel.Files))
-                    .Subscribe(notificationSource.SetData));
-            });
+            this.WhenAnyValue(x => x.ViewModel.Files)
+                .Merge(this.WhenAnyObservable(x => x.ViewModel.Files.Changed).Select(_ => ViewModel.Files))
+                .Subscribe(notificationSource.SetData);
         }
     }
 }
